a5ff221d4c5542ade47a57bf7bd4e865bceae7b1
CrisspunK <121061203+CrissHdzHrra@users.noreply.github.com>
2025-06-12 09:53:42 -0600

    Update peliculas.py

--- P2_2A_BIS/1_proyecto_peliculas/peliculas.py
+++ P2_2A_BIS/1_proyecto_peliculas/peliculas.py
@@ -29,3 +29,49 @@ def limpiarPeliculas():
     if resp=="si":
         peliculas.clear()
         print("¡La operacion se realizó con exito!")
+
+def buscarPeliculas():
+    borrarPantalla()
+    print("Buscar Peliculas")
+    pelicula_buscar=input("Ingresa el nombre de la pelicula a buscar").upper().strip()
+    if not pelicula_buscar in peliculas:
+        print("Esta pelicula no existe en el sistema")
+    else:
+        encontro=0
+        for i in range(0,len(peliculas)):
+            if pelicula_buscar==peliculas[i]:
+                print(f"La pelicula {pelicula_buscar}se encontro en el casillero: {i+1}")
+                encontro+=1
+        print(f"Tenemos {encontro} pelicula(s) con ese titulo")
+        print("La operacion se realizo con exito")
+
+def modificarPeliculas():
+        borrarPantalla()
+        print("Buscar Peliculas")
+        pelicula_buscar=input("Ingresa el nombre de la pelicula a buscar").upper().strip()
+        if not pelicula_buscar in peliculas:
+            print("Esta pelicula no existe en el sistema")
+        else:
+            for i in range(0,len(peliculas)):
+                resp=input("¿deseas actualizar la pelicula? Si/no").lower()
+                if resp=="si":
+                    peliculas[i]=input("Introduce el nuevo nombre de la pelicula").upper().strip()
+                    encontro+=1
+                    print("La operacion se realizo con exito!")
+    
+        print(f"Se modifico {encontro} peliculas con este titulo")
+
+def borrarPelicula():
+    borrarPantalla()
+    print("Borrar Peliculas")
+    pelicula_buscar=input("Ingresa el nombre de la pelicula a buscar").upper().strip()
+    if pelicula_buscar in peliculas:
+        for i in range(0,len(peliculas)):
+            resp=input("¿Desea quitar o borrar esta pelicula? Si/No").lower()
+            if resp=="si":
+                pelicula_buscar.remove[i]
+                print("La operacion se realizo con exito!")
+                print(f"La pelicula que se borro {pelicula_buscar} estaba en el casillero: {i+1}")
+    else:
+        print("Esta pelicula no existe en el sistema")
+    
