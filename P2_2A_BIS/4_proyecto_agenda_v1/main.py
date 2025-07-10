6b39307085d8905c17ee88876b6cd3b16c2433e9
CrisspunK <121061203+CrissHdzHrra@users.noreply.github.com>
2025-07-09 22:35:26 -0600

    main.py

--- P2_2A_BIS/4_proyecto_agenda_v1/main.py
+++ P2_2A_BIS/4_proyecto_agenda_v1/main.py
@@ -1,31 +1,37 @@
 import agenda
 
 
-
 def main():
-    agenda_contactos={}
-    opcion=True
+    agenda_contactos = {}
+    opcion = True
     while opcion:
         agenda.borrarPantalla()
         agenda.menuPrincipal()
+        opcion = input("\t\tElige una opcion: ").strip() # Added input to capture user choice
 
-        if opcion=="1":
+        if opcion == "1":
             agenda.agregarContacto(agenda_contactos)
             agenda.esperarTecla()
-        elif opcion=="2":
+        elif opcion == "2":
             agenda.mostrarContactos(agenda_contactos)
             agenda.esperarTecla()
-        elif opcion=="3":
-            agenda.buscar_contacto(agenda_contactos)
+        elif opcion == "3":
+            agenda.buscarContacto(agenda_contactos)
             agenda.esperarTecla()
-        elif opcion=="4":
+        elif opcion == "4":
+            agenda.modificarContacto(agenda_contactos) # New: Modificar contacto
+            agenda.esperarTecla()
+        elif opcion == "5":
+            agenda.borrarContacto(agenda_contactos) # New: Borrar contacto
+            agenda.esperarTecla()
+        elif opcion == "6":
             agenda.borrarPantalla()
             print("Programa Finalizado")
-            opcion=False
+            opcion = False
         else:
-            opcion=True
-            print("\t\t:::Opcion no valida, vuelva a intentarlo:::")
+            print("\t\t:::Opcion no valida, vuelva a intentarlo 🚫⚠️:::")
+            agenda.esperarTecla()
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
+
